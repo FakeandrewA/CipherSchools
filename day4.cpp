6e5a7e6e43fb31e216ea836924b62ec91afdd108
FakeandrewA <143100343+FakeandrewA@users.noreply.github.com>
2024-08-26 19:30:10 +0530

    Update day4.cpp

--- day4.cpp
+++ day4.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+//1_arithmetic orperations
 
+void op(int a,int b)
+{ 
+  cout<<"Addition: " <<a+b;
+cout<<"Subtraction: " <<a-b;
+cout<<"Multiplication: "<<a*b;
+  if (b!=0)
+cout<<"Division: " <<a/b;
+  else
+    cout<<"Undefined";
+  if(b!=0)
+cout<<"Modulus: "<<a%b;
+  else
+cout<<"Undefined";
+}
+
+//2_
