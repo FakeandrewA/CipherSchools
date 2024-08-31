aa027a3b2d10e19dc601a02bba50462d053c4ef0
FakeandrewA <143100343+FakeandrewA@users.noreply.github.com>
2024-08-31 10:53:58 +0530

    Update LinkedList using Struct.cpp

--- LinkedList using Struct.cpp
+++ LinkedList using Struct.cpp
@@ -1,64 +1,84 @@
-#include<iostream>
-#include<cstdlib>
+// Online C++ compiler to run C++ program online
+#include <iostream>
 using namespace std;
 struct Node
 {
     int data;
     Node* next;
-    
-    Node(int new_data)
+    Node(int d)
     {
-        data = new_data;
+        data = d;
         next = nullptr;
     }
+
 };
-void Traverse(Node* head)
+void traverse(Node* head)
 {
-    Node* current = head;
-    while(current != nullptr)
+    Node* curr = head;
+    while(curr!=nullptr)
     {
-        cout<< current->data<<" ";
-        current= current->next;
+        cout<<curr->data<<" ";
+        curr= curr->next;
     }
 }
-
-Node* insertAtStart(Node* head,int Value)
+Node* IAB(Node* head,int v)
 {
-    Node* newNode = new Node(Value);
+    Node* newNode = new Node(v);
     if(head==nullptr)
-    {
-        return newNode;
-    }
-    newNode->next = head;
+    return newNode;
+    
+    newNode->next= head;
     head = newNode;
-    
     return head;
 }
+Node* IAE(Node* head,int v)
+{
+    Node* newNode = new Node(v);
+    if(head == nullptr)
+    return newNode;
+    
+    Node* curr = head;
+    while(curr->next != nullptr)
+        curr= curr->next;
+    curr->next = newNode;
+    return head;
 
-Node* insertAtEnd(Node* head,int Value)
-{
-    Node* newNode = new Node(Value);
-    
-    if(head==nullptr)
-    {
-        return newNode;
-    }
-    
-    Node* current = head;
-    while(current->next!=nullptr)
-    {
-        current = current->next;
-    }
-    current->next = newNode;
-    return head;
 }
-int main()
+Node* IAP(Node* head,int pos,int v)
 {
-    Node* head;
-    head = new Node(1);
-    head->next = new Node(2);
-    head->next->next = new Node(3);
-    head = insertAtStart(head,10);
-    head=insertAtEnd(head,15);
-    Traverse(head);
+    Node* newNode = new Node(v);
+    if(head == nullptr)
+    return newNode;
+    if(pos==1)
+    {
+        head = IAB(head,v);
+        return head;
+    }
+    Node* curr = head;
+    for(int i = 1 ; i < pos-1 && curr->next!=nullptr ; i++ )
+    {
+        curr = curr->next;
+    }
+    if(curr->next!=nullptr)
+    {
+    newNode->next = curr->next;
+    curr->next = newNode;
+    return head;
+    }
+    else
+    {
+        curr->next = newNode;
+        return head;
+    }
+}
+int main() {
+    Node* head = nullptr;
+    head = IAB(head,10);
+    head = IAB(head,11);
+    head = IAB(head,12);
+    head = IAB(head,13);
+    head = IAP(head,7,9);
+    traverse(head);
+
+    return 0;
 }
