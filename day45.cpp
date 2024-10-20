e97d61cbbe861207f6378bc627d8e1d03c532afd
FakeandrewA <143100343+FakeandrewA@users.noreply.github.com>
2024-10-20 22:34:08 +0530

    Create day45.cpp

---
+++ day45.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+#include<bits/stdc++.h>
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int d)
+    {
+        data = d;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* ilo(int arr[],int i,int n)
+{
+    if(i>=n)return nullptr;
+    
+    Node* root = new Node(arr[i]);
+    
+    root->left = ilo(arr,i*2 +1,n);
+    root->right = ilo(arr,i*2 +2,n);
+    
+    return root;
+}
+void inorder(Node* root)
+{
+    if(root==nullptr)
+    return;
+    
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+    
+}
+void postorder(Node* root)
+{
+    if(root==nullptr)return;
+    
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+    
+}
+
+int main()
+{   int arr1[5]={1,2,3,4,5};
+    Node* root1 = ilo(arr1,0,5);
+    inorder(root1);
+    cout<<endl;
+    postorder(root1);
+  
+    
+}
