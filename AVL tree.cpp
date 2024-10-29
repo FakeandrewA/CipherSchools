3a6223a24c013d6b57414bd0b5fcbf4a52693f1b
FakeandrewA <143100343+FakeandrewA@users.noreply.github.com>
2024-10-29 17:39:02 +0530

    Update AVL tree.cpp

--- AVL tree.cpp
+++ AVL tree.cpp
@@ -4,83 +4,119 @@ using namespace std;
 struct Node
 {
     int data;
-    int b_f;
+    int h;
     Node* left;
     Node* right;
     Node(int d)
     {
-        data = d;
-        b_f = 0;
-        left = nullptr;
-        right = nullptr;
+        data=d;
+        h=1;
+        left=nullptr;
+        right=nullptr;
     }
 };
-void inorder(Node* root)
+
+int height(Node* root)
 {
-    if(root==nullptr)
-    return;
-    
-    inorder(root->left);
-    cout<<root->data<<" b:"<<root->b_f<<endl;
-    inorder(root->right);
-    
+    return root?root->h:0;
 }
-Node* RRR(Node* root)
-{       
-    Node* curr = root->right;
-    curr->b_f = 0;
-    
-    curr->left = new Node(root->data);
-    curr->left->b_f = 0;
-    
-    
-    curr->right->b_f = 0;
-    
-    delete root;
-    return curr;
-}
-Node* insertBST(Node* root, int v)
+int balance(Node* root)
 {
-    if(root==nullptr)
+    return root?height(root->left)-height(root->right):0;
+}
+Node* LL(Node* x)
+{
+    Node* y = x->right;
+    Node* T2 = y->left;
+    
+    //rotation
+    y->left = x;
+    x->right = T2;
+    
+    //update h
+    x->h = 1+max(height(x->left),height(x->right));
+    y->h = 1+max(height(y->left),height(y->right));
+    
+    return y;
+}
+Node* RR(Node* x)
+{
+    Node* y = x->left;
+    Node* T2 = y->right;
+    
+    //rotation 
+    y->right = x;
+    x->left = T2;
+    
+    //update h
+    x->h = 1+max(height(x->left),height(x->right));
+    y->h = 1+max(height(y->left),height(y->right));
+    
+    return y;
+}
+Node* insertAVL(Node* root,int v)
+{
+    if(root == nullptr)
     return new Node(v);
     
-    if(root->b_f == -2)
-    {
-        if(root->right->b_f == -1)
-        {
-            root = RRR(root);
-        }
-    }
-    
     if(root->data>v)
-    {
-        root->b_f+=1;
-        root->left = insertBST(root->left,v);
-    }
+    root->left = insertAVL(root->left,v);
+    else if(root->data<v)
+    root->right = insertAVL(root->right,v);
     else
+    return root;
+    
+    root->h = 1 + max(height(root->left),height(root->right));
+    
+    int b = balance(root);
+    
+    //LL case (RR rotation)
+    if(b>1 && root->left->data > v)
     {
-        root->b_f-=1;
-        root->right = insertBST(root->right,v);
-        
+        return RR(root);
+    }
+    //RR case (LL rotation)
+    if(b<-1 && root->right->data < v)
+    {
+        return LL(root);
+    }
+    //LR case (double rotation)
+    if(b>1 && root->left->data < v)
+    {
+        root->left = LL(root->left);
+        return RR(root);
+    }
+    
+    //RL case (double rotation)
+    if(b<-1 && root->right->data > v)
+    {
+        root->right = RR(root->right);
+        return LL(root);
     }
     
     return root;
 }
-Node* buildBST(int arr[],int n)
+Node* buildAVL(int arr[],int n)
 {
-    Node* root =nullptr;
+    Node* root = nullptr;
     for(int i=0;i<n;i++)
-    {
-        root=insertBST(root,arr[i]);
-    }
+    root = insertAVL(root,arr[i]);
+    
     return root;
 }
-
-
-int main() {
+void preorder(Node* root)
+{
+    if(root==nullptr)return;
     
-    int arr[7]={5,1,2,3,4,6,7};
-    Node* root = nullptr;
-    root = buildBST(arr,5);
-    inorder(root);
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main()
+{
+    int arr[10]={5,1,2,3,4,6,7,8,9,0};
+    Node* root=nullptr;
+    root= buildAVL(arr,10);
+    preorder(root);
 }
